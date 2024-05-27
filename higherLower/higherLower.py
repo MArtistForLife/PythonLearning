8ccfd64e018e7d4eb7e737318e2b8c5dc024df6d
MArtistForLife <maiyuri.artist@gmail.com>
2024-05-26 23:55:26 -0400

    pov: programmer next to me debugged this in like 2 minutes ;_;

---
+++ higherLower/higherLower.py
@@ -0,0 +1,382 @@
+##---------------------##
+print("EXERCISE 1")
+print("This is for a game of comparison, called Higher or Lower.")
+
+#all the data we're gonna need 
+data = [
+    {
+        'name': 'Instagram',
+        'follower_count': 346,
+        'description': 'Social media platform',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cristiano Ronaldo',
+        'follower_count': 215,
+        'description': 'Footballer',
+        'country': 'Portugal'
+    },
+    {
+        'name': 'Ariana Grande',
+        'follower_count': 183,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Dwayne Johnson',
+        'follower_count': 181,
+        'description': 'Actor and professional wrestler',
+        'country': 'United States'
+    },
+    {
+        'name': 'Selena Gomez',
+        'follower_count': 174,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kylie Jenner',
+        'follower_count': 172,
+        'description': 'Reality TV personality and businesswoman and Self-Made Billionaire',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kim Kardashian',
+        'follower_count': 167,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Lionel Messi',
+        'follower_count': 149,
+        'description': 'Footballer',
+        'country': 'Argentina'
+    },
+    {
+        'name': 'Beyoncé',
+        'follower_count': 145,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Neymar',
+        'follower_count': 138,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'National Geographic',
+        'follower_count': 135,
+        'description': 'Magazine',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Bieber',
+        'follower_count': 133,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Taylor Swift',
+        'follower_count': 131,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kendall Jenner',
+        'follower_count': 127,
+        'description': 'Reality TV personality and Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Jennifer Lopez',
+        'follower_count': 119,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Nicki Minaj',
+        'follower_count': 113,
+        'description': 'Musician',
+        'country': 'Trinidad and Tobago'
+    },
+    {
+        'name': 'Nike',
+        'follower_count': 109,
+        'description': 'Sportswear multinational',
+        'country': 'United States'
+    },
+    {
+        'name': 'Khloé Kardashian',
+        'follower_count': 108,
+        'description': 'Reality TV personality and businesswoman',
+        'country': 'United States'
+    },
+    {
+        'name': 'Miley Cyrus',
+        'follower_count': 107,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': 'Katy Perry',
+        'follower_count': 94,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kourtney Kardashian',
+        'follower_count': 90,
+        'description': 'Reality TV personality',
+        'country': 'United States'
+    },
+    {
+        'name': 'Kevin Hart',
+        'follower_count': 89,
+        'description': 'Comedian and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Ellen DeGeneres',
+        'follower_count': 87,
+        'description': 'Comedian',
+        'country': 'United States'
+    },
+    {
+        'name': 'Real Madrid CF',
+        'follower_count': 86,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'FC Barcelona',
+        'follower_count': 85,
+        'description': 'Football club',
+        'country': 'Spain'
+    },
+    {
+        'name': 'Rihanna',
+        'follower_count': 81,
+        'description': 'Musician and businesswoman',
+        'country': 'Barbados'
+    },
+    {
+        'name': 'Demi Lovato',
+        'follower_count': 80,
+        'description': 'Musician and actress',
+        'country': 'United States'
+    },
+    {
+        'name': "Victoria's Secret",
+        'follower_count': 69,
+        'description': 'Lingerie brand',
+        'country': 'United States'
+    },
+    {
+        'name': 'Zendaya',
+        'follower_count': 68,
+        'description': 'Actress and musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Shakira',
+        'follower_count': 66,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Drake',
+        'follower_count': 65,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Chris Brown',
+        'follower_count': 64,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'LeBron James',
+        'follower_count': 63,
+        'description': 'Basketball player',
+        'country': 'United States'
+    },
+    {
+        'name': 'Vin Diesel',
+        'follower_count': 62,
+        'description': 'Actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'Cardi B',
+        'follower_count': 67,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'David Beckham',
+        'follower_count': 82,
+        'description': 'Footballer',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Billie Eilish',
+        'follower_count': 61,
+        'description': 'Musician',
+        'country': 'United States'
+    },
+    {
+        'name': 'Justin Timberlake',
+        'follower_count': 59,
+        'description': 'Musician and actor',
+        'country': 'United States'
+    },
+    {
+        'name': 'UEFA Champions League',
+        'follower_count': 58,
+        'description': 'Club football competition',
+        'country': 'Europe'
+    },
+    {
+        'name': 'NASA',
+        'follower_count': 56,
+        'description': 'Space agency',
+        'country': 'United States'
+    },
+    {
+        'name': 'Emma Watson',
+        'follower_count': 56,
+        'description': 'Actress',
+        'country': 'United Kingdom'
+    },
+    {
+        'name': 'Shawn Mendes',
+        'follower_count': 57,
+        'description': 'Musician',
+        'country': 'Canada'
+    },
+    {
+        'name': 'Virat Kohli',
+        'follower_count': 55,
+        'description': 'Cricketer',
+        'country': 'India'
+    },
+    {
+        'name': 'Gigi Hadid',
+        'follower_count': 54,
+        'description': 'Model',
+        'country': 'United States'
+    },
+    {
+        'name': 'Priyanka Chopra Jonas',
+        'follower_count': 53,
+        'description': 'Actress and musician',
+        'country': 'India'
+    },
+    {
+        'name': '9GAG',
+        'follower_count': 52,
+        'description': 'Social media platform',
+        'country': 'China'
+    },
+    {
+        'name': 'Ronaldinho',
+        'follower_count': 51,
+        'description': 'Footballer',
+        'country': 'Brasil'
+    },
+    {
+        'name': 'Maluma',
+        'follower_count': 50,
+        'description': 'Musician',
+        'country': 'Colombia'
+    },
+    {
+        'name': 'Camila Cabello',
+        'follower_count': 49,
+        'description': 'Musician',
+        'country': 'Cuba'
+    },
+    {
+        'name': 'NBA',
+        'follower_count': 47,
+        'description': 'Club Basketball Competition',
+        'country': 'United States'
+    }
+]
+
+import random
+
+def game():
+    gameOver= False #easy boolean that will only turn True if a wrong answer is given
+    userScore = 0
+    usedIndices = set() #set to keep track of used indices
+
+    indexA = random.randint(0, len(data) - 1)
+    indexB = random.randint(0, len(data) - 1)
+    while indexA == indexB:
+        indexB = random.randint(0, len(data) - 1)
+
+    usedIndices.add(indexA)
+    usedIndices.add(indexB)
+
+    while not gameOver:
+        choiceA = data[indexA]
+        choiceB = data[indexB]
+
+        # Ensure choiceB becomes choiceA in every round
+        choiceA, choiceB = choiceB, choiceA
+
+        print(f"\nCompare...!\nA~ {choiceA['name']}: {choiceA['description']} from {choiceA['country']}.")
+        print("versus...")
+        print(f"B~ {choiceB['name']}: {choiceB['description']} from {choiceB['country']}.")
+
+        while True:
+            try:
+                more = input("Who has more followers? Type 'A' or 'B': ").upper()
+
+                if more == "A":
+                    if choiceA['follower_count'] > choiceB['follower_count']: #if B is smaller
+                        print(f"Correct! {choiceA['name']} has {choiceA['follower_count']} million followers, while {choiceB['name']} has {choiceB['follower_count']} million.")
+                        userScore += 1
+                        print(f"Your current score is {userScore}.")
+                        
+                        while True:
+                            indexB = random.randint(0, len(data) - 1) 
+                            if indexB != indexA and indexB not in usedIndices: 
+                                usedIndices.add(indexB) #B is moving forward because it is smaller, it's been used now
+                                break #get out of this while loop 
+                        break
+                    else:
+                        print(f"Wrong! Actually, {choiceB['name']} has {choiceB['follower_count']} million followers, while {choiceA['name']} has {choiceA['follower_count']} million.")
+                        gameOver = True
+                        break
+
+                elif more == "B":
+                    if choiceB['follower_count'] > choiceA['follower_count']:
+                        print(f"Correct! {choiceB['name']} has {choiceB['follower_count']} million followers, while {choiceA['name']} has {choiceA['follower_count']} million.")
+                        userScore += 1
+                        print(f"Your current score is {userScore}.")
+                        
+                        while True:
+                            indexA = random.randint(0, len(data) - 1) 
+                            if indexA != indexB and indexA not in usedIndices: 
+                                usedIndices.add(indexA) #A is moving forward because it is smaller, so it has been "used" now
+                                break #get out of this while loop
+                        break
+                    else:
+                        print(f"Wrong! Actually, {choiceA['name']} has {choiceA['follower_count']} million followers, while {choiceB['name']} has {choiceB['follower_count']} million.")
+                        gameOver = True
+                        break
+                else:
+                    raise ValueError #explicitly calls out that an error's been made so we need to go to ValueError
+            except ValueError:
+                print("Whoops, that's not an option! Try again. Just type 'A' or 'B' to make your guess.")
+
+    if gameOver:
+        print(f"Game over! Your final score is {userScore}.")
+        # else:
+        #     break #get out of the outer while loop, meaning the initial 2 picks (outside BOTH while loops) would still be used AGAIN
+
+game()
